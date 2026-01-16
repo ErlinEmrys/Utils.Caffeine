f6007c7c4d1c55c0f15771869bfd680ceea2740a
Erlin Emrys <Erlin@seznam.cz>
2026-01-16 11:01:26 +0000

    About form

--- Erlin.Utils.Caffeine/About.Designer.cs
+++ Erlin.Utils.Caffeine/About.Designer.cs
@@ -35,10 +35,18 @@ partial class About
 	{
 		webBrowserLicense = new System.Windows.Forms.WebBrowser();
 		tabControl1 = new System.Windows.Forms.TabControl();
+		tabPage3 = new System.Windows.Forms.TabPage();
+		tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+		tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+		labelTitle = new System.Windows.Forms.Label();
+		labelVersion = new System.Windows.Forms.Label();
 		tabPage1 = new System.Windows.Forms.TabPage();
 		tabPage2 = new System.Windows.Forms.TabPage();
 		webBrowserThirdParties = new System.Windows.Forms.WebBrowser();
 		tabControl1.SuspendLayout();
+		tabPage3.SuspendLayout();
+		tableLayoutPanel1.SuspendLayout();
+		tableLayoutPanel2.SuspendLayout();
 		tabPage1.SuspendLayout();
 		tabPage2.SuspendLayout();
 		SuspendLayout();
@@ -53,6 +61,7 @@ partial class About
 		// 
 		// tabControl1
 		// 
+		tabControl1.Controls.Add( tabPage3 );
 		tabControl1.Controls.Add( tabPage1 );
 		tabControl1.Controls.Add( tabPage2 );
 		tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -62,8 +71,72 @@ partial class About
 		tabControl1.Size = new System.Drawing.Size( 800, 450 );
 		tabControl1.TabIndex = 1;
 		// 
+		// tabPage3
+		// 
+		tabPage3.BackColor = System.Drawing.Color.FromArgb( ( ( int )( ( byte )34 ) ), ( ( int )( ( byte )34 ) ), ( ( int )( ( byte )34 ) ) );
+		tabPage3.Controls.Add( tableLayoutPanel1 );
+		tabPage3.ForeColor = System.Drawing.Color.White;
+		tabPage3.Location = new System.Drawing.Point( 4, 28 );
+		tabPage3.Name = "tabPage3";
+		tabPage3.Size = new System.Drawing.Size( 792, 418 );
+		tabPage3.TabIndex = 2;
+		tabPage3.Text = "About";
+		// 
+		// tableLayoutPanel1
+		// 
+		tableLayoutPanel1.ColumnCount = 1;
+		tableLayoutPanel1.ColumnStyles.Add( new System.Windows.Forms.ColumnStyle( System.Windows.Forms.SizeType.Percent, 100F ) );
+		tableLayoutPanel1.Controls.Add( tableLayoutPanel2, 0, 1 );
+		tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+		tableLayoutPanel1.Location = new System.Drawing.Point( 0, 0 );
+		tableLayoutPanel1.Name = "tableLayoutPanel1";
+		tableLayoutPanel1.RowCount = 3;
+		tableLayoutPanel1.RowStyles.Add( new System.Windows.Forms.RowStyle( System.Windows.Forms.SizeType.Percent, 33.333332F ) );
+		tableLayoutPanel1.RowStyles.Add( new System.Windows.Forms.RowStyle( System.Windows.Forms.SizeType.Percent, 33.333332F ) );
+		tableLayoutPanel1.RowStyles.Add( new System.Windows.Forms.RowStyle( System.Windows.Forms.SizeType.Percent, 33.333332F ) );
+		tableLayoutPanel1.Size = new System.Drawing.Size( 792, 418 );
+		tableLayoutPanel1.TabIndex = 0;
+		// 
+		// tableLayoutPanel2
+		// 
+		tableLayoutPanel2.ColumnCount = 1;
+		tableLayoutPanel2.ColumnStyles.Add( new System.Windows.Forms.ColumnStyle( System.Windows.Forms.SizeType.Percent, 50F ) );
+		tableLayoutPanel2.Controls.Add( labelTitle, 0, 0 );
+		tableLayoutPanel2.Controls.Add( labelVersion, 0, 1 );
+		tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+		tableLayoutPanel2.Location = new System.Drawing.Point( 3, 142 );
+		tableLayoutPanel2.Name = "tableLayoutPanel2";
+		tableLayoutPanel2.RowCount = 2;
+		tableLayoutPanel2.RowStyles.Add( new System.Windows.Forms.RowStyle( System.Windows.Forms.SizeType.Percent, 50F ) );
+		tableLayoutPanel2.RowStyles.Add( new System.Windows.Forms.RowStyle( System.Windows.Forms.SizeType.Percent, 50F ) );
+		tableLayoutPanel2.Size = new System.Drawing.Size( 786, 133 );
+		tableLayoutPanel2.TabIndex = 0;
+		// 
+		// labelTitle
+		// 
+		labelTitle.Dock = System.Windows.Forms.DockStyle.Fill;
+		labelTitle.Font = new System.Drawing.Font( "Segoe UI", 36.313046F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ( ( byte )0 ) );
+		labelTitle.Location = new System.Drawing.Point( 3, 0 );
+		labelTitle.Name = "labelTitle";
+		labelTitle.Size = new System.Drawing.Size( 780, 66 );
+		labelTitle.TabIndex = 0;
+		labelTitle.Text = "label1";
+		labelTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+		// 
+		// labelVersion
+		// 
+		labelVersion.Dock = System.Windows.Forms.DockStyle.Fill;
+		labelVersion.Font = new System.Drawing.Font( "Segoe UI", 18.156523F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ( ( byte )0 ) );
+		labelVersion.Location = new System.Drawing.Point( 3, 66 );
+		labelVersion.Name = "labelVersion";
+		labelVersion.Size = new System.Drawing.Size( 780, 67 );
+		labelVersion.TabIndex = 1;
+		labelVersion.Text = "label2";
+		labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+		// 
 		// tabPage1
 		// 
+		tabPage1.BackColor = System.Drawing.Color.FromArgb( ( ( int )( ( byte )34 ) ), ( ( int )( ( byte )34 ) ), ( ( int )( ( byte )34 ) ) );
 		tabPage1.Controls.Add( webBrowserLicense );
 		tabPage1.Location = new System.Drawing.Point( 4, 28 );
 		tabPage1.Name = "tabPage1";
@@ -71,25 +144,24 @@ partial class About
 		tabPage1.Size = new System.Drawing.Size( 792, 418 );
 		tabPage1.TabIndex = 0;
 		tabPage1.Text = "License";
-		tabPage1.UseVisualStyleBackColor = true;
 		// 
 		// tabPage2
 		// 
+		tabPage2.BackColor = System.Drawing.Color.FromArgb( ( ( int )( ( byte )34 ) ), ( ( int )( ( byte )34 ) ), ( ( int )( ( byte )34 ) ) );
 		tabPage2.Controls.Add( webBrowserThirdParties );
 		tabPage2.Location = new System.Drawing.Point( 4, 28 );
 		tabPage2.Name = "tabPage2";
 		tabPage2.Padding = new System.Windows.Forms.Padding( 3 );
-		tabPage2.Size = new System.Drawing.Size( 280, 194 );
+		tabPage2.Size = new System.Drawing.Size( 792, 418 );
 		tabPage2.TabIndex = 1;
-		tabPage2.Text = "Third parties";
-		tabPage2.UseVisualStyleBackColor = true;
+		tabPage2.Text = "Third parties licenses";
 		// 
 		// webBrowserThirdParties
 		// 
 		webBrowserThirdParties.Dock = System.Windows.Forms.DockStyle.Fill;
 		webBrowserThirdParties.Location = new System.Drawing.Point( 3, 3 );
 		webBrowserThirdParties.Name = "webBrowserThirdParties";
-		webBrowserThirdParties.Size = new System.Drawing.Size( 274, 188 );
+		webBrowserThirdParties.Size = new System.Drawing.Size( 786, 412 );
 		webBrowserThirdParties.TabIndex = 1;
 		// 
 		// About
@@ -100,10 +172,22 @@ partial class About
 		Controls.Add( tabControl1 );
 		Text = "About";
 		tabControl1.ResumeLayout( false );
+		tabPage3.ResumeLayout( false );
+		tableLayoutPanel1.ResumeLayout( false );
+		tableLayoutPanel2.ResumeLayout( false );
 		tabPage1.ResumeLayout( false );
 		tabPage2.ResumeLayout( false );
 		ResumeLayout( false );
 	}
+
+	private System.Windows.Forms.Label labelTitle;
+	private System.Windows.Forms.Label labelVersion;
+
+	private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+
+	private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+
+	private System.Windows.Forms.TabPage tabPage3;
 
 	private System.Windows.Forms.WebBrowser webBrowserLicense;
 
--- Erlin.Utils.Caffeine/About.cs
+++ Erlin.Utils.Caffeine/About.cs
@@ -7,7 +7,7 @@ using Markdig;
 namespace Erlin.Utils.Caffeine;
 
 /// <summary>
-/// About form
+///    About form
 /// </summary>
 public partial class About : Form
 {
@@ -18,6 +18,9 @@ public partial class About : Form
 		InitializeComponent();
 		Icon = Resources.Caffeine;
 		Text = "About " + CurrentAssemly.GetName().Name;
+
+		labelTitle.Text = CurrentAssemly.GetName().Name;
+		labelVersion.Text = CurrentAssemly.GetName().Version?.ToString();
 	}
 
 	protected override void OnLoad( EventArgs e )
@@ -29,18 +32,56 @@ public partial class About : Form
 	}
 
 	/// <summary>
-	/// Loads embeded license file
+	///    Loads embeded license file
 	/// </summary>
 	private string GetEmbededFile( string resourceName )
 	{
 		using Stream? stream = CurrentAssemly.GetManifestResourceStream( resourceName );
-		if ( stream != null )
+		if( stream != null )
 		{
 			using StreamReader reader = new( stream );
 			string content = reader.ReadToEnd();
-			return Markdown.ToHtml( content );
+			return About.EncapsulateHtml( Markdown.ToHtml( content ) );
 		}
 
 		return string.Empty;
+	}
+
+	/// <summary>
+	///    Provides html and css encapsulation for html segment
+	/// </summary>
+	private static string EncapsulateHtml( string htmlBody )
+	{
+		return $$"""
+				<!doctype html>
+				<html lang="en">
+					<head>
+						<meta charset="utf-8" />
+						<meta name="viewport" content="width=device-width, initial-scale=1" />
+						<style>
+							body {
+								background-color: #222;
+								color: white;
+							}
+							blockquote {
+								background-color: #333;
+								padding: 0.5em;
+								border-radius: 0.5em;
+								border: 1px solid #666;
+								box-shadow: 0.25rem 0.25rem 0 #666;
+							}
+							blockquote blockquote {
+								background-color: #444;
+							}
+							blockquote blockquote blockquote {
+								background-color: #555;
+							}
+						</style>
+					</head>
+					<body>
+						{{htmlBody}}
+					</body>
+				</html>
+				""";
 	}
 }
